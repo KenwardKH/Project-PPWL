204364d2f6787fc90fadf208f753d319f27aa297
KenwardKH <kenwardkh5@gmail.com>
2024-05-17 00:03:46 +0700

    Kontak

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -530,9 +530,10 @@ https://templatemo.com/tm-583-festava-live
                                             <span>Rp{{ number_format($jadwal->harga, 0, ',', '.') }}</span>
                                             <span>{{ $jadwal->lokasi }}</span>
                                             @auth
-                                            <a href="{{ route('beli_tiket', $jadwal->id)}}" class="btn custom-btn">Buy Ticket</a>
+                                                <a href="{{ route('beli_tiket', $jadwal->id) }}"
+                                                    class="btn custom-btn">Buy Ticket</a>
                                             @else
-                                            <button onclick="buy_ticket()" class="btn custom-btn">Buy Ticket</button>
+                                                <button onclick="buy_ticket()" class="btn custom-btn">Buy Ticket</button>
                                             @endauth
                                         </li>
                                     @endforeach
@@ -639,73 +640,134 @@ https://templatemo.com/tm-583-festava-live
 
 
         <section class="contact-section section-padding" id="section_6">
-            <div class="container">
-                <div class="row">
+            @auth
+                <div class="container">
+                    <div class="row">
 
-                    <div class="col-lg-8 col-12 mx-auto">
-                        <h2 class="text-center mb-4">Interested? Let's talk</h2>
+                        <div class="col-lg-8 col-12 mx-auto">
+                            <h2 class="text-center mb-4">Interested? Let's talk</h2>
 
-                        <nav class="d-flex justify-content-center">
-                            <div class="nav nav-tabs align-items-baseline justify-content-center" id="nav-tab"
-                                role="tablist">
-                                <button class="nav-link active" id="nav-ContactForm-tab" data-bs-toggle="tab"
-                                    data-bs-target="#nav-ContactForm" type="button" role="tab"
-                                    aria-controls="nav-ContactForm" aria-selected="false">
-                                    <h5>Contact Form</h5>
-                                </button>
+                            <nav class="d-flex justify-content-center">
+                                <div class="nav nav-tabs align-items-baseline justify-content-center" id="nav-tab"
+                                    role="tablist">
+                                    <button class="nav-link active" id="nav-ContactForm-tab" data-bs-toggle="tab"
+                                        data-bs-target="#nav-ContactForm" type="button" role="tab"
+                                        aria-controls="nav-ContactForm" aria-selected="false">
+                                        <h5>Contact Form</h5>
+                                    </button>
 
-                                <button class="nav-link" id="nav-ContactMap-tab" data-bs-toggle="tab"
-                                    data-bs-target="#nav-ContactMap" type="button" role="tab"
-                                    aria-controls="nav-ContactMap" aria-selected="false">
-                                    <h5>Google Maps</h5>
-                                </button>
-                            </div>
-                        </nav>
+                                    <button class="nav-link" id="nav-ContactMap-tab" data-bs-toggle="tab"
+                                        data-bs-target="#nav-ContactMap" type="button" role="tab"
+                                        aria-controls="nav-ContactMap" aria-selected="false">
+                                        <h5>Google Maps</h5>
+                                    </button>
+                                </div>
+                            </nav>
 
-                        <div class="tab-content shadow-lg mt-5" id="nav-tabContent">
-                            <div class="tab-pane fade show active" id="nav-ContactForm" role="tabpanel"
-                                aria-labelledby="nav-ContactForm-tab">
-                                <form class="custom-form contact-form mb-5 mb-lg-0" action="#" method="post"
-                                    role="form">
-                                    <div class="contact-form-body">
-                                        <div class="row">
-                                            <div class="col-lg-6 col-md-6 col-12">
-                                                <input type="text" name="contact-name" id="contact-name"
-                                                    class="form-control" placeholder="Full name" required>
+                            <div class="tab-content shadow-lg mt-5" id="nav-tabContent">
+                                <div class="tab-pane fade show active" id="nav-ContactForm" role="tabpanel"
+                                    aria-labelledby="nav-ContactForm-tab">
+                                    <div class="custom-form contact-form mb-5 mb-lg-0">
+                                        <div class="contact-form-body">
+                                            <div class="row">
+                                                <div class="col-lg-6 col-md-6 col-12">
+                                                    <input type="text" name="contact-name" id="contact-name"
+                                                        class="form-control" placeholder="Full name" required>
+                                                </div>
+
+                                                <div class="col-lg-6 col-md-6 col-12">
+                                                    <input type="email" name="contact-email" id="contact-email"
+                                                        pattern="[^ @]*@[^ @]*" class="form-control"
+                                                        placeholder="Email address" required readonly
+                                                        value="{{ Auth::user()->email }}">
+                                                </div>
                                             </div>
 
-                                            <div class="col-lg-6 col-md-6 col-12">
-                                                <input type="email" name="contact-email" id="contact-email"
-                                                    pattern="[^ @]*@[^ @]*" class="form-control"
-                                                    placeholder="Email address" required>
+                                            <textarea name="contact-message" rows="3" class="form-control" id="contact-message" placeholder="Message"></textarea>
+
+                                            <div class="col-lg-4 col-md-10 col-8 mx-auto">
+                                                <button onclick="sendMail()" class="form-control">Send message</button>
                                             </div>
-                                        </div>
-
-                                        <input type="text" name="contact-company" id="contact-company"
-                                            class="form-control" placeholder="Company" required>
-
-                                        <textarea name="contact-message" rows="3" class="form-control" id="contact-message" placeholder="Message"></textarea>
-
-                                        <div class="col-lg-4 col-md-10 col-8 mx-auto">
-                                            <button type="submit" class="form-control">Send message</button>
                                         </div>
                                     </div>
-                                </form>
-                            </div>
+                                </div>
 
-                            <div class="tab-pane fade" id="nav-ContactMap" role="tabpanel"
-                                aria-labelledby="nav-ContactMap-tab">
-                                <iframe class="google-map"
-                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29974.469402870927!2d120.94861466021855!3d14.106066818082482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd777b1ab54c8f%3A0x6ecc514451ce2be8!2sTagaytay%2C%20Cavite%2C%20Philippines!5e1!3m2!1sen!2smy!4v1670344209509!5m2!1sen!2smy"
-                                    width="100%" height="450" style="border:0;" allowfullscreen=""
-                                    loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                                <!-- You can easily copy the embed code from Google Maps -> Share -> Embed a map // -->
+                                <div class="tab-pane fade" id="nav-ContactMap" role="tabpanel"
+                                    aria-labelledby="nav-ContactMap-tab">
+                                    <iframe class="google-map"
+                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29974.469402870927!2d120.94861466021855!3d14.106066818082482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd777b1ab54c8f%3A0x6ecc514451ce2be8!2sTagaytay%2C%20Cavite%2C%20Philippines!5e1!3m2!1sen!2smy!4v1670344209509!5m2!1sen!2smy"
+                                        width="100%" height="450" style="border:0;" allowfullscreen=""
+                                        loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    <!-- You can easily copy the embed code from Google Maps -> Share -> Embed a map // -->
+                                </div>
                             </div>
                         </div>
-                    </div>
 
+                    </div>
                 </div>
-            </div>
+            @else
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-8 col-12 mx-auto">
+                            <nav class="d-flex justify-content-center">
+                                <div class="nav nav-tabs align-items-baseline justify-content-center" id="nav-tab"
+                                    role="tablist">
+                                    <button class="nav-link active" id="nav-ContactForm-tab" data-bs-toggle="tab"
+                                        data-bs-target="#nav-ContactForm" type="button" role="tab"
+                                        aria-controls="nav-ContactForm" aria-selected="false">
+                                        <h5>Contact Form</h5>
+                                    </button>
+
+                                    <button class="nav-link" id="nav-ContactMap-tab" data-bs-toggle="tab"
+                                        data-bs-target="#nav-ContactMap" type="button" role="tab"
+                                        aria-controls="nav-ContactMap" aria-selected="false">
+                                        <h5>Google Maps</h5>
+                                    </button>
+                                </div>
+                            </nav>
+
+                            <div class="tab-content shadow-lg mt-5" id="nav-tabContent">
+                                <div class="tab-pane fade show active" id="nav-ContactForm" role="tabpanel"
+                                    aria-labelledby="nav-ContactForm-tab">
+                                    <div class="custom-form contact-form mb-5 mb-lg-0">
+                                        <div class="contact-form-body">
+                                            <div class="row">
+                                                <div class="col-lg-6 col-md-6 col-12">
+                                                    <input type="text" name="contact-name" id="contact-name"
+                                                        class="form-control" placeholder="Full name" required>
+                                                </div>
+
+                                                <div class="col-lg-6 col-md-6 col-12">
+                                                    <input type="email" name="contact-email" id="contact-email"
+                                                        pattern="[^ @]*@[^ @]*" class="form-control"
+                                                        placeholder="Email address" required>
+                                                </div>
+                                            </div>
+
+                                            <textarea name="contact-message" rows="3" class="form-control" id="contact-message" placeholder="Message"></textarea>
+
+                                            <div class="col-lg-4 col-md-10 col-8 mx-auto">
+                                                <button onclick="noEmail()" class="form-control">Send message</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="tab-pane fade" id="nav-ContactMap" role="tabpanel"
+                                    aria-labelledby="nav-ContactMap-tab">
+                                    <iframe class="google-map"
+                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29974.469402870927!2d120.94861466021855!3d14.106066818082482!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd777b1ab54c8f%3A0x6ecc514451ce2be8!2sTagaytay%2C%20Cavite%2C%20Philippines!5e1!3m2!1sen!2smy!4v1670344209509!5m2!1sen!2smy"
+                                        width="100%" height="450" style="border:0;" allowfullscreen=""
+                                        loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    <!-- You can easily copy the embed code from Google Maps -> Share -> Embed a map // -->
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            @endauth
         </section>
     </main>
 
@@ -858,9 +920,33 @@ https://templatemo.com/tm-583-festava-live
 T e m p l a t e M o
 
 -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
+    <script type="text/javascript">
+        (function() {
+            emailjs.init("FRBle9ocLF-9uGEAk");
+        })();
+    </script>
     <script>
-        function buy_ticket(){
+        function sendMail() {
+            var params = {
+                from_name: document.getElementById("contact-name").value,
+                message: document.getElementById("contact-message").value,
+                email: document.getElementById("contact-email").value,
+            }
+            emailjs.send("service_pwx12bn", "template_ihslo6e", params).then(function(res) {
+                alert("Email Berhasil Dikirim! ");
+                location.reload();
+            })
+        }
+    </script>
+
+    <script>
+        function buy_ticket() {
             alert('Login terlebih dahulu untuk membeli tiket')
+        }
+
+        function noEmail() {
+            alert('Login terlebih dahulu untuk mengirim email')
         }
 
         document.addEventListener("DOMContentLoaded", function() {
