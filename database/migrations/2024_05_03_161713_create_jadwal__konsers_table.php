2e7253854202431ce91e0048d559493e4d0bea71
Drakenvei <nicholaskensurya@gmail.com>
2024-05-17 11:34:06 +0700

    testfix

--- database/migrations/2024_05_03_161713_create_jadwal__konsers_table.php
+++ database/migrations/2024_05_03_161713_create_jadwal__konsers_table.php
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('jadwal__konsers');
+        Schema::dropIfExists('jadwal_konser');
     }
 };
